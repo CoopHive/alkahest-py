32d3691423790503fde95b4b7e4b5fce9dadd432
疒奀 <yz@mmon.co>
2025-01-17 12:04:01 +0800

    bundle

--- src/lib.rs
+++ src/lib.rs
@@ -828,3 +828,69 @@ impl Erc1155Client {
         Ok(receipt.transaction_hash.to_string())
     }
 }
+
+#[pymethods]
+impl TokenBundleClient {
+    pub async fn collect_payment(
+        &self,
+        buy_attestation: String,
+        fulfillment: String,
+    ) -> eyre::Result<String> {
+        let receipt = self
+            .inner
+            .collect_payment(buy_attestation.parse()?, fulfillment.parse()?)
+            .await?;
+        Ok(receipt.transaction_hash.to_string())
+    }
+
+    pub async fn collect_expired(&self, buy_attestation: String) -> eyre::Result<String> {
+        let receipt = self.inner.collect_expired(buy_attestation.parse()?).await?;
+        Ok(receipt.transaction_hash.to_string())
+    }
+
+    pub async fn buy_with_bundle(
+        &self,
+        price: TokenBundleData,
+        item: ArbiterData,
+        expiration: u64,
+    ) -> eyre::Result<String> {
+        let receipt = self
+            .inner
+            .buy_with_bundle(price.try_into()?, item.try_into()?, expiration)
+            .await?;
+        Ok(receipt.transaction_hash.to_string())
+    }
+
+    pub async fn pay_with_bundle(
+        &self,
+        price: TokenBundleData,
+        payee: String,
+    ) -> eyre::Result<String> {
+        let receipt = self
+            .inner
+            .pay_with_bundle(price.try_into()?, payee.parse()?)
+            .await?;
+        Ok(receipt.transaction_hash.to_string())
+    }
+
+    pub async fn buy_bundle_for_bundle(
+        &self,
+        bid: TokenBundleData,
+        ask: TokenBundleData,
+        expiration: u64,
+    ) -> eyre::Result<String> {
+        let receipt = self
+            .inner
+            .buy_bundle_for_bundle(bid.try_into()?, ask.try_into()?, expiration)
+            .await?;
+        Ok(receipt.transaction_hash.to_string())
+    }
+
+    pub async fn pay_bundle_for_bundle(&self, buy_attestation: String) -> eyre::Result<String> {
+        let receipt = self
+            .inner
+            .pay_bundle_for_bundle(buy_attestation.parse()?)
+            .await?;
+        Ok(receipt.transaction_hash.to_string())
+    }
+}
