bbd6f5a6c2595c7c3e5a7baef4852a0d48329898
疒奀 <yz@mmon.co>
2025-01-22 17:15:12 +0800

    return LogWithHash

--- src/clients/token_bundle.rs
+++ src/clients/token_bundle.rs
@@ -2,7 +2,10 @@ use alkahest_rs::clients::token_bundle;
 use pyo3::{pyclass, pymethods};
 use tokio::runtime::Runtime;
 
-use crate::types::{ArbiterData, TokenBundleData};
+use crate::{
+    get_attested_event,
+    types::{ArbiterData, AttestedLog, LogWithHash, TokenBundleData},
+};
 
 #[pyclass]
 #[derive(Clone)]
@@ -44,13 +47,16 @@ impl TokenBundleClient {
         price: TokenBundleData,
         item: ArbiterData,
         expiration: u64,
-    ) -> eyre::Result<String> {
+    ) -> eyre::Result<LogWithHash<AttestedLog>> {
         Runtime::new()?.block_on(async {
             let receipt = self
                 .inner
                 .buy_with_bundle(price.try_into()?, item.try_into()?, expiration)
                 .await?;
-            Ok(receipt.transaction_hash.to_string())
+            Ok(LogWithHash {
+                log: get_attested_event(receipt.clone())?.data.into(),
+                transaction_hash: receipt.transaction_hash.to_string(),
+            })
         })
     }
 
@@ -58,13 +64,16 @@ impl TokenBundleClient {
         &self,
         price: TokenBundleData,
         payee: String,
-    ) -> eyre::Result<String> {
+    ) -> eyre::Result<LogWithHash<AttestedLog>> {
         Runtime::new()?.block_on(async {
             let receipt = self
                 .inner
                 .pay_with_bundle(price.try_into()?, payee.parse()?)
                 .await?;
-            Ok(receipt.transaction_hash.to_string())
+            Ok(LogWithHash {
+                log: get_attested_event(receipt.clone())?.data.into(),
+                transaction_hash: receipt.transaction_hash.to_string(),
+            })
         })
     }
 
@@ -73,23 +82,32 @@ impl TokenBundleClient {
         bid: TokenBundleData,
         ask: TokenBundleData,
         expiration: u64,
-    ) -> eyre::Result<String> {
+    ) -> eyre::Result<LogWithHash<AttestedLog>> {
         Runtime::new()?.block_on(async {
             let receipt = self
                 .inner
                 .buy_bundle_for_bundle(bid.try_into()?, ask.try_into()?, expiration)
                 .await?;
-            Ok(receipt.transaction_hash.to_string())
+            Ok(LogWithHash {
+                log: get_attested_event(receipt.clone())?.data.into(),
+                transaction_hash: receipt.transaction_hash.to_string(),
+            })
         })
     }
 
-    pub async fn pay_bundle_for_bundle(&self, buy_attestation: String) -> eyre::Result<String> {
+    pub async fn pay_bundle_for_bundle(
+        &self,
+        buy_attestation: String,
+    ) -> eyre::Result<LogWithHash<AttestedLog>> {
         Runtime::new()?.block_on(async {
             let receipt = self
                 .inner
                 .pay_bundle_for_bundle(buy_attestation.parse()?)
                 .await?;
-            Ok(receipt.transaction_hash.to_string())
+            Ok(LogWithHash {
+                log: get_attested_event(receipt.clone())?.data.into(),
+                transaction_hash: receipt.transaction_hash.to_string(),
+            })
         })
     }
 }
