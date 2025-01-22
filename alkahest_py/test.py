59aa8493fdfadbae5c14043661a53c9c15252e18
疒奀 <yz@mmon.co>
2025-01-22 17:05:02 +0800

    update example

--- alkahest_py/test.py
+++ alkahest_py/test.py
@@ -8,12 +8,18 @@ client = AlkahestClient(
 
 
 async def main():
-    hash = await client.erc20.approve(
-        {"address": "0x036CbD53842c5426634e7929541eC2318f3dCF7e", "value": 100},
-        "escrow",
-    )
+    # hash = await client.erc20.approve(
+    #     {"address": "0x036CbD53842c5426634e7929541eC2318f3dCF7e", "value": 100},
+    #     "escrow",
+    # )
+    # print(hash)
 
-    print(hash)
+    escrow = await client.erc20.buy_with_erc20(
+        {"address": "0x036CbD53842c5426634e7929541eC2318f3dCF7e", "value": 100},
+        {"arbiter": "0x0000000000000000000000000000000000000000", "demand": b""},
+        0,
+    )
+    print(escrow)
 
 
 if __name__ == "__main__":
